13f3d14694c80d60ad5de6708184f3becbd542af
JORTIZ <jesusmoh89@gmail.com>
2023-03-19 23:54:46 -0400

    dockerfile and springboot

--- demospringboot/src/main/java/com/jesusmoh/demospringboot/DemospringbootApplication.java
+++ demospringboot/src/main/java/com/jesusmoh/demospringboot/DemospringbootApplication.java
@@ -2,12 +2,21 @@ package com.jesusmoh.demospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class DemospringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemospringbootApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+
 
 }
