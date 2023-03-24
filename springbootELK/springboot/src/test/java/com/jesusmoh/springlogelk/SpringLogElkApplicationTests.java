94bf8d9c46731b4e8dffca5431935d4c40a32614
JORTIZ <jesusmoh89@gmail.com>
2023-03-24 16:41:37 -0400

    springbootELK

---
+++ springbootELK/springboot/src/test/java/com/jesusmoh/springlogelk/SpringLogElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusmoh.springlogelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
