19b545a5badd56886f65aebad17f501799354466
JORTIZ <jesusmoh89@gmail.com>
2023-03-28 23:22:53 -0400

    springbootREDIS

---
+++ springbootREDIS/src/main/java/com/jesusmoh/demoredis/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.jesusmoh.demoredis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("172.17.0.1", 6379));
+    }
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
