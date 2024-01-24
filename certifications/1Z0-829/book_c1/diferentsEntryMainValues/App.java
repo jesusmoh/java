18d921e3d2f4586cb0ac14b51519ee98df48d1d9
jortiz <jesusmoh89@gmail.com>
2024-01-23 21:31:47 -0500

    book_c1

--- certifications/1Z0-829/book_c1/diferentsEntryMainValues/App.java
+++ certifications/1Z0-829/book_c1/diferentsEntryMainValues/App.java
@@ -2,8 +2,8 @@
 public class App {
 
     // java App.java frutas
-    public final static void main(String[] args) {
+    public final static void main(String[] argssssssss) {
 
-        System.out.println("Hello World " + " " + args[0]);
+        System.out.println("Hello World " + " " + argssssssss[0]);
     }
 }
